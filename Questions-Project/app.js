ffa5a62d4299f0402daad8617190d93652074aa9
abhi17-96 <abhi.jadhav0396@gmail.com>
2023-10-21 19:44:11 +0530

    Add html and js

--- Questions-Project/app.js
+++ Questions-Project/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+
+questions.forEach((ques) => {
+    const btn = ques.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+
+        questions.forEach((question) => {
+            if(question !== ques) {
+                question.classList.remove("show-text");
+            }           
+        })
+
+        ques.classList.toggle("show-text");
+    })
+})
+
+
 // traversing the dom
+// const qBtn = document.querySelectorAll('.question-btn');
+
+// qBtn.forEach((btn) => btn.addEventListener("click", function(e) {
+//     const ques = e.currentTarget.parentElement.parentElement;
+//     ques.classList.toggle('show-text');
+    
+// }))
